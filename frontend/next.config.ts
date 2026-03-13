798f63d79a7a0109269b5d802a005518ad09bcc3
Bhuvan <bhuvan@example.com>
2026-03-14 01:40:58 +0530

    Include out folder

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/aim-ops-1",
 };
 
 export default nextConfig;
